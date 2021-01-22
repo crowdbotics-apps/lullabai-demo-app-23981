26dc2da574a5d7f7665911c8ae477642b7401d3c
Crowdbotics <team@crowdbotics.com>
2021-01-22 05:05:52 +0000

    Update models

--- backend/lullabai_demo_app_23981/wsgi.py
+++ backend/lullabai_demo_app_23981/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lullabai_demo_app_23981.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lullabai_demo_app_23981.settings")
 
 application = get_wsgi_application()
